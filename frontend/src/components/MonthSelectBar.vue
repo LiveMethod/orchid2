27a7e727a3f7865263e7e72f3e938d2e8f8d34fd
RICK STEELE <patrickjsteele@gmail.com>
2017-08-21 23:34:00 -0700

    comitting these vue components before blowing them away forever

---
+++ frontend/src/components/MonthSelectBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="month-select-bar">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonthSelectBar',
+  data () {
+    return {
+      msg: 'Well, aint this some shit'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.month-select-bar{
+  h1 {
+    font-weight: normal;
+    color: red;
+  }
+}
+</style>
